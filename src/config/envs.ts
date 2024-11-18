5cd3c53ecf9387202a6ffbdf78f8b8d46bba43c9
Israel <israel.pe@outlook.es>
2024-11-18 16:34:18 -0500

    feat: added db host and sync

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import * as env from 'env-var';
 
 export const envs = {
@@ -6,5 +7,7 @@ export const envs = {
   dbUser: env.get('DB_USER').required(true).asString(),
   dbPass: env.get('DB_PASS').required(true).asString(),
   dbPort: env.get('DB_PORT').required(true).asPortNumber(),
+  dbHost: env.get('DB_HOST').required(true).asString(),
+  dbSync: env.get('DB_SYNC').required(true).asBool(),
   jwtSeed: env.get('JWT_SEED').required(true).asString(),
 };

a96c1fae905fdfa992ef50b50b4b1b90c7e2b8a3
Israel <israel.pe@outlook.es>
2025-06-30 18:23:16 -0500

    files faltantes

--- src/modules/attendance/dto/search-attendance.dto.ts
+++ src/modules/attendance/dto/search-attendance.dto.ts
@@ -1,1 +1,19 @@
-export class SearchAttendanceDto
+import { Expose } from 'class-transformer';
+import { IsDateString, IsNumberString, IsOptional } from 'class-validator';
+
+export class SearchAttendanceDto {
+  @Expose()
+  @IsOptional()
+  @IsNumberString()
+  memberId: number;
+
+  @Expose()
+  @IsOptional()
+  @IsDateString()
+  startDate: Date;
+
+  @Expose()
+  @IsOptional()
+  @IsDateString()
+  endDate: Date;
+}
